b41797d9ff824b795559e5b23259a5a6ef6b29de
Mohammad Sadman <41444793+kaneki666@users.noreply.github.com>
2019-09-19 12:41:46 +0600

    Add files via upload

---
+++ instgram_bot.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import time
+
+class Instagram:
+	def __init__(self,username,password):
+		self.username = username
+		self.password = password
+		self.bot = webdriver.Chrome()
+
+	def login(self):
+		bot = self.bot
+		bot.get('https://www.instagram.com/accounts/login/')
+		time.sleep(2)
+		email = bot.find_element_by_name('username')
+		password = bot.find_element_by_name('password')
+		email.clear()
+		password.clear()
+		email.send_keys(self.username)
+		password.send_keys(self.password)
+		password.send_keys(Keys.RETURN)
+		time.sleep(3)
+		not_now = bot.find_element_by_class_name('aOOlW')
+		not_now.click()
+		time.sleep(3)
+		comment = bot.find_element_by_class_name('Ypffh')
+		comment.send_keys("SELENIUM IS FUN")
+		post = bot.find_element_by_xpath('//*[@id="react-root"]/section/main/section/div[1]/div[1]/div/article[1]/div[2]/section[3]/div/form/button')
+		post.send_keys(Keys.RETURN)
+		heart = bot.find_element_by_class_name('dCJp8')
+		heart.click()
+sadman = Instagram('your twitter id','twitter password')
+sadman.login()
